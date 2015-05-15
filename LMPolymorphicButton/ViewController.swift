602b3b42cdcd13051fb46e4b226c5ad9bafbee92
lastMove <lastMove@me.com>
2015-05-15 20:48:23 +0200

    Initial Commit

---
+++ LMPolymorphicButton/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LMPolymorphicButton
+//
+//  Created by jason akakpo on 15/05/2015.
+//  Copyright (c) 2015 MTT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
